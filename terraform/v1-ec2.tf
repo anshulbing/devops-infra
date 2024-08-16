30e2d165f83f3cb3f9c6524d0531e6b441bad4ad
anshulbing <sansh786.as@gmail.com>
2024-08-16 17:10:23 +0530

    added ec2, vpc, sg, igw and rt

--- terraform/v1-ec2.tf
+++ terraform/v1-ec2.tf
@@ -8,4 +8,101 @@ provider "aws" {
 resource "aws_instance" "testEC2" {
   ami = "ami-0ba9883b710b05ac6"
   instance_type = "t2.micro"
+  key_name = "udemydemokey"
+  # security_groups = ["demo-sg"] when launching in a vpc this is not required instead use vpc_sg_id
+  vpc_security_group_ids = [aws_security_group.demo-sg.id]
+  subnet_id = aws_subnet.dpp-public-subnet-1.id
+
+  tags = {
+    Name = "dpp-ec2" }
+  
+}
+
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc.id
+
+  ingress {
+    description      = "Shh access"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+
+  }
+}
+
+resource "aws_vpc" "dpp-vpc" {
+  cidr_block       = "10.1.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "dpp-vpc"
+  }
+}
+
+resource "aws_subnet" "dpp-public-subnet-1" {
+  vpc_id     = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "dpp-public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "dpp-public-subnet-2" {
+  vpc_id     = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "dpp-public-subnet-2"
+  }
+}
+
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id = aws_vpc.dpp-vpc.id
+
+  tags = {
+    Name = "dpp-igw"
+  }
+}
+
+resource "aws_route_table" "dpp-public-rt" {
+  vpc_id = aws_vpc.dpp-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp-igw.id
+  }
+
+  tags = {
+    Name = "dpp-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-1" {
+  subnet_id      = aws_subnet.dpp-public-subnet-1.id
+  route_table_id = aws_route_table.dpp-public-rt.id
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-2" {
+  subnet_id      = aws_subnet.dpp-public-subnet-2.id
+  route_table_id = aws_route_table.dpp-public-rt.id
 }
